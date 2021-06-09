72e768960f3c2e0209acd03a1cfdf8e77b42a88a
Huzaifa181 <huzaifa.ahmed181@gmail.com>
2021-06-10 03:52:30 +0500

    make the api of post and get messages and setup the pusher.js for realtime messages shown on the frontend as well trigger on tjhe backend

--- WhatsApp Backend/server.js
+++ WhatsApp Backend/server.js
@@ -1,10 +1,23 @@
 import express from 'express'
 import mongoose from 'mongoose'
+import Messages from './dbMessages.js'
+import  Pusher from "pusher";
+import cors from 'cors'
 
 const app=express();
 const port = process.env.port || 9000
 
-const connection_url=`mongodb+srv://Huzaifa:AB2xBprKsCDvYOL1@cluster0.dqjdi.mongodb.net/whatsapp-clone?retryWrites=true&w=majority`
+const pusher = new Pusher({
+    appId: "1217146",
+    key: "81e0491a168617f6f216",
+    secret: "4d102dfcb8c80f9e512c",
+    cluster: "ap2",
+    useTLS: true
+  });
+
+  app.use(cors())
+  
+const connection_url=`mongodb+srv://Huzaifa:wIB1aqsU7M7NNtxD@cluster0.dqjdi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
 
 mongoose.connect(connection_url,{
     useCreateIndex:true,
@@ -12,7 +25,53 @@ mongoose.connect(connection_url,{
     useUnifiedTopology:true
 })
 
+const db=mongoose.connection;
+db.once("open",()=>{
+    console.log("DB connected");
+
+    const msgCollection=db.collection("messagecontents");
+    const changeStream=msgCollection.watch();
+    changeStream.on("change",change=>{
+        if(change.operationType==='insert'){
+            const messageDetails=change.fullDocument
+            console.log(messageDetails)
+            pusher.trigger("message", "inserted",{
+                name:messageDetails.name,
+                message:messageDetails.message
+            })
+        }
+        else{
+            console.log("Error Trigginf Pusher")
+        }
+    })
+})
+
+//middlewares
+app.use(express.json())
+
 app.get("/",(req,res)=>res.status(200).send("hello world"));
+
+app.get("/messages/sync",(req,res)=>{
+    Messages.find((err,data)=>{
+        if(err){
+            res.status(500).send(data)
+        }
+        else{
+            res.status(200).send(data)
+        }
+    })
+})
+app.post("/messages/new",(req,res)=>{
+    const dbMessage=req.body;
+    Messages.create(dbMessage,(err,data)=>{
+        if(err){
+            res.status(500).send(data)
+        }
+        else{
+            res.status(201).send(data)
+        }
+    })
+})
 
 //listen
 app.listen(port,()=>console.log(`listning on local host :${port}`))
--- WhatsApp Frontend/src/App.js
+++ WhatsApp Frontend/src/App.js
@@ -1,11 +1,22 @@
 import React, { useEffect } from "react";
 import {BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import logo from './logo.svg';
+import Pusher from 'pusher-js';
 import 'antd/dist/antd.css';
 import Sidebar from './Sidebar'
 import Chat from './Chat'
 import './App.css'
 function App() {
+  useEffect(()=>{
+    const pusher = new Pusher('81e0491a168617f6f216', {
+      cluster: 'ap2'
+    });
+
+    var channel = pusher.subscribe('inserted');
+    channel.bind('messages', function(data) {
+      alert(JSON.stringify(data));
+    });
+  })
   return (
     <div className='app'>
         <div className='app__body'>
