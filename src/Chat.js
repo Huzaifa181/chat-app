eb3185cd5119a7147b1ea9be15474d726ab9e152
Huzaifa181 <huzaifa.ahmed181@gmail.com>
2021-06-10 00:11:50 +0500

    Creating structure of chat body where all messages wil be shown for the paricular user

--- src/Chat.js
+++ src/Chat.js
@@ -1,9 +1,41 @@
 import React from 'react'
+import {Avatar,IconButton} from '@material-ui/core';
+import DonutLargeIcon from '@material-ui/icons/DonutLarge';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import ChatIcon from '@material-ui/icons/Chat';
+import SearchOutlinedIcon from '@material-ui/icons/SearchOutlined';
 import './Chat.css'
 const Chat = () => {
     return (
         <div className='chat'>
-            <h1>I am chat box</h1>
+           <div className='chat__header'>
+            <Avatar/>
+            
+            <div className='chat__headerInfo'>
+                <h3>Room name</h3>
+                <p>Last Seen at</p>
+            </div>
+            <div className='chat__headerRight'>
+            <IconButton>
+                    <DonutLargeIcon/>
+                    </IconButton>
+                    <IconButton>
+                        <ChatIcon/>
+                    </IconButton>
+                    <IconButton>
+                        <MoreVertIcon/>
+                    </IconButton>
+                </div>
+            </div>
+            <div className='chat__body'>
+                <p className='chat__message'>
+                    <span className='chat__name'>Huzaifa</span>
+                    This is a message
+                    <span className="chat__timestamp">
+                        {new Date().toUTCString()}
+                    </span>
+                </p>
+            </div>
         </div>
     )
 }
