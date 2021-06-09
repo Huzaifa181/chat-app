9e7024bfec89031b7b3ce5603ada623977ef393c
Huzaifa181 <huzaifa.ahmed181@gmail.com>
2021-06-09 22:48:40 +0500

    Make the side =bar component with header icons and search bar and chat component to show user name

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -2,24 +2,40 @@ import React from 'react'
 import DonutLargeIcon from '@material-ui/icons/DonutLarge';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
 import ChatIcon from '@material-ui/icons/Chat';
-import {IconButton} from '@material-ui/core';
+import SearchOutlinedIcon from '@material-ui/icons/SearchOutlined';
+import {Avatar,IconButton} from '@material-ui/core';
+import SideBarChat from './SideBarChat'
 import './Sidebar.css'
 const Sidebar = () => {
     return ( 
         <div className = 'sidebar' >
             <div className="sidebar__header">
-                
-            <div className="sidebar__headerRight">
-                <IconButton>
-                    <DonutLargeIcon/>
-                </IconButton>
-                <IconButton>
-                    <ChatIcon/>
-                </IconButton>
-                <IconButton>
-                    <MoreVertIcon/>
-                </IconButton>
+                <Avatar/>
+                <div className="sidebar__headerRight">
+                    <IconButton>
+                        <DonutLargeIcon/>
+                    </IconButton>
+                    <IconButton>
+                        <ChatIcon/>
+                    </IconButton>
+                    <IconButton>
+                        <MoreVertIcon/>
+                    </IconButton>
+                </div>
             </div>
+            <div className="sidebar__search">
+                <div className='sidebar__searchContainer'>
+                    <SearchOutlinedIcon/>
+                    <input placeholder="Search or Start Now chat" type="text"/>
+                </div>
+            </div>
+            <div className="sidebar__chats">
+                <SideBarChat/>
+                <SideBarChat/>
+                <SideBarChat/>
+                <SideBarChat/>
+                <SideBarChat/>
+                <SideBarChat/>
             </div>
         </div>
     )
